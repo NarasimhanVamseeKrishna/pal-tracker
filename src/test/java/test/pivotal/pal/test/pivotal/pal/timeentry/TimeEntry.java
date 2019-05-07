aa1b3f74d6ecd84a1e7b12919ac4c740b71e868c
vnarasi5 <vamseekrishna1989@gmail.com>
2019-05-07 09:34:51 -0600

    Added TimeEntry class

---
+++ src/test/java/test/pivotal/pal/test/pivotal/pal/timeentry/TimeEntry.java
@@ -0,0 +1,12 @@
+package test.pivotal.pal.test.pivotal.pal.timeentry;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+}
